219f3817da2a5afe20354a84326ccc03d92e982e
kontrast7 <47153257+kontrast7@users.noreply.github.com>
2021-09-16 14:40:21 +0300

    Updates

---
+++ precache-manifest.4c9e9d1879e67ec8d5644dc933227f4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f650dddac3b536e17c38ebb2174a3e4",
+    "url": "/ignat-tasks/index.html"
+  },
+  {
+    "revision": "bffc7cf9917f55b573ff",
+    "url": "/ignat-tasks/static/css/main.3ae8f578.chunk.css"
+  },
+  {
+    "revision": "71df0b9f4744b1724b56",
+    "url": "/ignat-tasks/static/js/2.5251a31e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ignat-tasks/static/js/2.5251a31e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bffc7cf9917f55b573ff",
+    "url": "/ignat-tasks/static/js/main.a8579d60.chunk.js"
+  },
+  {
+    "revision": "3485a8263cbf503c284f",
+    "url": "/ignat-tasks/static/js/runtime-main.b3787de6.js"
+  }
+]);
