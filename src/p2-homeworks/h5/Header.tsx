66719fb10806db13d8d124f4e3e6a6d79e46e35c
kontrast7 <47153257+kontrast7@users.noreply.github.com>
2021-09-16 18:41:12 +0300

    hw5

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { PATH } from "./Routes";
 
 function Header() {
-    return (
-        <div>
-            // add NavLinks
-
-        </div>
-    )
+  return (
+    <div>
+      <NavLink to={PATH.PRE_JUNIOR}>pre_Junior</NavLink>
+      <NavLink to={PATH.JUNIOR}>Junior</NavLink>
+      <NavLink to={PATH.JUNIOR_PLUS}>Junior_Plus</NavLink>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
