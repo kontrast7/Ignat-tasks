2f9096550c44f1c947445725adcc1fdc28d40818
kontrast7 <47153257+kontrast7@users.noreply.github.com>
2021-10-15 17:52:37 +0300

    7-8hw

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,41 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': {
-            // need to fix
-            return state
-        }
-        case 'check': {
-            // need to fix
-            return state
-        }
-        default: return state
+import { UserType } from "../HW8";
+
+export const homeWorkReducer = (
+  state: UserType[],
+  action: AllType
+): UserType[] => {
+  switch (action.type) {
+    case "sort": {
+      const newState = [...state].sort((a, b) => {
+        let nameA = a.name.toLowerCase();
+        let nameB = b.name.toLowerCase();
+        if (nameA < nameB) return -1;
+        else if (nameA > nameB) return 1;
+        else return 0;
+      });
+      return action.payload === "up" ? newState : newState.reverse();
     }
-}
+    case "check": {
+      return state.filter((f) => f.age >= action.payload);
+    }
+    default:
+      return state;
+  }
+};
+
+export type AllType = SortACType | CheckACType;
+export type SortACType = ReturnType<typeof SortAC>;
+export type CheckACType = ReturnType<typeof CheckAC>;
+
+export const SortAC = (payload: string) => {
+  return {
+    type: "sort",
+    payload,
+  } as const;
+};
+export const CheckAC = (payload: number) => {
+  return {
+    type: "check",
+    payload,
+  } as const;
+};
