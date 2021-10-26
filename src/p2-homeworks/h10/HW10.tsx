859a5c8d041d557595f92436a0afa0e84ea9bac9
kontrast7 <47153257+kontrast7@users.noreply.github.com>
2021-10-26 18:44:57 +0300

    hw 10

--- src/p2-homeworks/h10/HW10.tsx
+++ src/p2-homeworks/h10/HW10.tsx
@@ -1,38 +1,41 @@
-import React from 'react'
-import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import React from "react";
+import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch } from "redux";
+import { AppStoreType } from "./bll/store";
+import { loadingAC, loadingACType } from "./bll/loadingReducer";
+import load from "./bll/load.gif";
 
 function HW10() {
-    // useSelector, useDispatch
-    const loading = false
+  const dispatch = useDispatch<Dispatch<loadingACType>>();
+  const selectAllState = (store: AppStoreType) => store.loading;
+  const { loading } = useSelector(selectAllState);
 
-    const setLoading = () => {
-        // dispatch
-        // setTimeout
-        console.log('loading...')
-    };
+  const setLoading = () => {
+    dispatch(loadingAC(true));
+    setTimeout(() => dispatch(loadingAC(false)), 2000);
+  };
 
-    return (
+  return (
+    <div>
+      <hr />
+      homeworks 10
+      {/*should work (должно работать)*/}
+      {loading ? (
         <div>
-            <hr/>
-            homeworks 10
-
-            {/*should work (должно работать)*/}
-            {loading
-                ? (
-                    <div>крутилка...</div>
-                ) : (
-                    <div>
-                        <SuperButton onClick={setLoading}>set loading...</SuperButton>
-                    </div>
-                )
-            }
-
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<Alternative/>*/}
-            <hr/>
+          <img src={load} alt={"load"}/>
         </div>
-    )
+      ) : (
+        <div>
+          <SuperButton onClick={setLoading}>set loading...</SuperButton>
+        </div>
+      )}
+      <hr />
+      {/*для личного творчества, могу проверить*/}
+      {/*<Alternative/>*/}
+      <hr />
+    </div>
+  );
 }
 
-export default HW10
+export default HW10;
